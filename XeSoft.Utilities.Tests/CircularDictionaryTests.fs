dc19b1dddb091785c2a7a5bf32e1feeb78b51530
kspeakman <kasey@cornerspeed.com>
2015-09-11 00:28:47 -0500

    added CircularDictionary, gave up on IDictionary support for now since f# explicit interface upcast is so irritating, breaking all my tests

---
+++ XeSoft.Utilities.Tests/CircularDictionaryTests.fs
@@ -0,0 +1,132 @@
+﻿namespace TrainSafe.Data.Service.Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open System
+open System.Linq
+open XeSoft.Utilities
+
+[<TestClass>]
+type CircularDictionaryTests () = 
+    let testGuids = [ for i in 1 .. 10 -> (Guid.NewGuid(), i.ToString()) ]
+
+    [<TestMethod>]
+    member __.``Circular Dictionary default indexer works`` () = 
+        let cd = new CircularDictionary<Guid, string>(3)
+        for i in 0 .. 2 do
+            let (key, value) = testGuids.[i]
+            cd.Add(key, value)
+            Assert.AreEqual(value, cd.[key])
+
+    [<TestMethod>]
+    member __.``Circular Dictionary Count works`` () = 
+        let cd = new CircularDictionary<Guid, string>(3)
+        Assert.AreEqual(0, cd.Count)
+        for i in 0 .. 2 do
+            let (key, value) = testGuids.[i]
+            cd.Add(key, value)
+            Assert.AreEqual(i + 1, cd.Count)
+
+    [<TestMethod>]
+    member __.``Circular Dictionary ContainsKey works`` () = 
+        let cd = new CircularDictionary<Guid, string>(3)
+        for i in 0 .. 2 do
+            let (key, value) = testGuids.[i]
+            Assert.AreEqual(false, cd.ContainsKey(key))
+            cd.Add(key, value)
+            Assert.AreEqual(true, cd.ContainsKey(key))
+
+    [<TestMethod>]
+    member __.``Circular Dictionary Keys works`` () = 
+        let cd = new CircularDictionary<Guid, string>(3)
+        for i in 0 .. 2 do
+            let (key, value) = testGuids.[i]
+            cd.Add(key, value)
+            Assert.AreEqual(key, cd.Keys.Skip(i).First())
+
+    [<TestMethod>]
+    member __.``Circular Dictionary Values works`` () = 
+        let cd = new CircularDictionary<Guid, string>(3)
+        for i in 0 .. 2 do
+            let (key, value) = testGuids.[i]
+            cd.Add(key, value)
+            Assert.AreEqual(value, cd.Values.Skip(i).First())
+
+    [<TestMethod>]
+    member __.``Circular Dictionary observes capacity limit and overwrites oldest values`` () = 
+        let cd = new CircularDictionary<Guid, string>(3)
+        Assert.AreEqual(0, cd.Count)
+        cd.Add(fst testGuids.[0], snd testGuids.[0])
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[0]))
+        Assert.AreEqual(1, cd.Count)
+        cd.Add(fst testGuids.[1], snd testGuids.[1])
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[0]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[1]))
+        Assert.AreEqual(2, cd.Count)
+        cd.Add(fst testGuids.[2], snd testGuids.[2])
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[0]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[1]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[2]))
+        Assert.AreEqual(3, cd.Count)
+        cd.Add(fst testGuids.[3], snd testGuids.[3])
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[0]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[1]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[2]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[3]))
+        Assert.AreEqual(3, cd.Count)
+        cd.Add(fst testGuids.[4], snd testGuids.[4])
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[0]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[1]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[2]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[3]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[4]))
+        Assert.AreEqual(3, cd.Count)
+        cd.Add(fst testGuids.[5], snd testGuids.[5])
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[0]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[1]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[2]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[3]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[4]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[5]))
+        Assert.AreEqual(3, cd.Count)
+        cd.Add(fst testGuids.[6], snd testGuids.[6])
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[0]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[1]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[2]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[3]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[4]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[5]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[6]))
+        Assert.AreEqual(3, cd.Count)
+        cd.Add(fst testGuids.[7], snd testGuids.[7])
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[0]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[1]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[2]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[3]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[4]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[5]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[6]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[7]))
+        Assert.AreEqual(3, cd.Count)
+        cd.Add(fst testGuids.[8], snd testGuids.[8])
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[0]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[1]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[2]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[3]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[4]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[5]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[6]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[7]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[8]))
+        Assert.AreEqual(3, cd.Count)
+        cd.Add(fst testGuids.[9], snd testGuids.[9])
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[0]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[1]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[2]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[3]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[4]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[5]))
+        Assert.IsFalse(cd.ContainsKey(fst testGuids.[6]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[7]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[8]))
+        Assert.IsTrue(cd.ContainsKey(fst testGuids.[9]))
+        Assert.AreEqual(3, cd.Count)
---
+++ XeSoft.Utilities/CircularDictionary.fs
@@ -0,0 +1,75 @@
+﻿namespace XeSoft.Utilities
+
+open System.Collections.Generic
+
+/// A combination dictionary and circular buffer.
+/// When the dictionary is full, any new additions will overwrite the oldest entry.
+type CircularDictionary<'key, 'value when 'key : equality>(capacity: int) =
+
+    let dict = new System.Collections.Generic.Dictionary<'key, 'value>(capacity)
+    let maxIndex = capacity - 1
+    let buffer = Array.zeroCreate<'key> capacity
+    let mutable isFull : bool = false
+    let mutable next : int = 0
+
+    //interface System.Collections.Generic.IDictionary<'key, 'value> with
+    /// Add the key and value to the dictionary.
+    /// When capacity is reached, subsequent additions will overwrite the oldest entry.
+    member __.Add (key:'key, value:'value) =
+        let put () =
+            dict.Add (key, value)
+            buffer.[next] <- key
+        
+        let moveNext () =
+            if next = maxIndex then
+                isFull <- true
+                next <- 0
+            else
+                next <- next + 1
+
+        let clear () =
+            dict.Remove (buffer.[next])
+            |> ignore
+
+        if isFull then clear ()
+        put ()
+        moveNext ()
+
+    /// Add the key value pair to the dictionary.
+    /// When capacity is reached, subsequent additions will overwrite the oldest entry.
+    //member me.Add (kvp:KeyValuePair<'key, 'value>) =
+    //    (me :> IDictionary<'key, 'value>).Add(kvp.Key, kvp.Value)
+
+    // ------------ parroting standard dictionary properties ------------
+
+    /// Gets the value associated with the specified key
+    member __.Item
+        with get (key:'key) = dict.Item (key)
+        and set _ _ = raise (new System.NotSupportedException())
+    /// Get the number of key/value pairs contained in the circular dictionary
+    member __.Count with get () = dict.Count
+    /// Determines whether the circular dictionary contains the specified key
+    member __.ContainsKey (key:'key) = dict.ContainsKey (key)
+    /// Determines whether the circular dictionary contains the specified key value pair
+    member __.Contains (item:KeyValuePair<'key, 'value>) = dict.ContainsKey (item.Key)
+    /// Gets a collection containing the keys in the circular dictionary
+    member __.Keys with get () = dict.Keys :> ICollection<'key>
+    /// Gets a collection containing the values in the circular dictionary
+    member __.Values with get () = dict.Values :> ICollection<'value>
+    /// Gets the value associated with the specified key
+    member __.TryGetValue (key:'key, value:byref<'value>) = dict.TryGetValue (key, ref value)
+        
+    member __.GetEnumerator () : IEnumerator<KeyValuePair<'key,'value>> = dict.GetEnumerator() :> IEnumerator<KeyValuePair<'key,'value>>
+    //member __.GetEnumerator () : System.Collections.IEnumerator = (dict :> System.Collections.IEnumerable).GetEnumerator()
+
+
+    member __.IsReadOnly with get () = false
+    /// Remove is not supported.
+    member __.Remove (_:'key) : bool = raise (new System.NotSupportedException())
+    /// Remove is not supported.
+    member __.Remove (_:KeyValuePair<'key,'value>) : bool = raise (new System.NotSupportedException())
+    /// Clear is not supported.
+    member __.Clear () : unit = raise (new System.NotSupportedException())
+    /// CopyTo is not supported.
+    member __.CopyTo (_, _) : unit = raise (new System.NotSupportedException())
+
