4599e71db1b6dcc693d4898e7d7d345cf2e84deb
Kasey Speakman <kasey@cornerspeed.com>
2017-03-06 08:47:18 -0600

    added scale explanation, simplified

--- XeSoft.Utilities/Ratio.fs
+++ XeSoft.Utilities/Ratio.fs
@@ -4,8 +4,8 @@ module Ratio =
 
     open System
 
-    /// Scale the given Double between a and b
-    /// scale should be a Double between 0.0 and 1.0 inclusive of both
+    /// Scale the given ratio between a and b
+    /// scale should be between 0.0 and 1.0, can be inclusive of both
     let scaleBetween (a:int) (b:int) (scale:float) =
         let (min, max) =
             if a > b
@@ -15,15 +15,18 @@ module Ratio =
         then min
         else
             let scale = Math.Abs scale // negative scale doesn't make sense here
-            if scale > 1.0 // scale greater than 1 doesn't make sense
+            if scale >= 1.0 // scale greater than 1 doesn't make sense
             then max
             else
+                // visualize min = 0, max = 3, dots being all floats between
+                // | 0 . . . | 1 . . . | 2 . . . | 3
+                // | 0 . . . | 1 . . . | 2 . . . | 3 . . . | 4
+                // must scale to 4 and floor to actually get fair scaling from 0 to 3
+                // 1.0 scale is thrown out above, so not possible to get 4
                 let values = max - min
                 let scaled =
                     float (values + 1) * scale // generate a float on continuum from 0 <-> values + 1
                     |> Math.Floor // move float down to closest integer, 
                     |> Convert.ToInt32 // convert to int32 for compatibility with array index
-                // infinitesimal chance for values + 1, but account for it
-                let scaled = if scaled > values then values else scaled
                 scaled + min // shift scaled up to minimum
 
